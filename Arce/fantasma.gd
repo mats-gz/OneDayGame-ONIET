d4970597c4aa3d407b09b133f834baf8c4c44cf7
matiass :3 <matiasguzmanrodriguez89@gmail.com>
2025-10-16 14:13:49 -0300

    Carpeta Arce

---
+++ Arce/fantasma.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+signal hit
+var velocity = 40
+var direction = 1
+
+@onready var ray_cast_der: RayCast2D = $RayCastDer
+@onready var ray_cast_izq: RayCast2D = $RayCastIzq
+@onready var animacion_sprite: AnimatedSprite2D = $Ghost_2D
+
+func _ready() -> void:
+	add_to_group("ghost")
+
+func _process(delta: float) -> void:
+	if ray_cast_der.is_colliding():
+		direction = -1
+		animacion_sprite.flip_h = true
+		print("me golpie")
+	if ray_cast_izq.is_colliding():
+		direction = 1
+		animacion_sprite.flip_h = false
+		print("me golpie")
+		
+	position.x += delta * velocity * direction
+	
+	animacion_sprite.play("Idle")
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
